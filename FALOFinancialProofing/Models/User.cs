680ea8ef94335cead683ed3ed570bcd8ababc4d0
thanhpc1 <thanhpc1@newwave.vn>
2024-09-18 01:44:03 +0700

    config general repository

--- FALOFinancialProofing/Models/User.cs
+++ FALOFinancialProofing/Models/User.cs
@@ -1,12 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using FALOFinancialProofing.Core;
+using System.ComponentModel.DataAnnotations;
 
 namespace FALOFinancialProofing.Models
 {
-    public class User
+    public class User : Entity
     {
         public User() { }
-        [Key]
-        public int Id { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
