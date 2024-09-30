b8bd5c242b6b9ba1677f1887e43cd2bfff9981bc
thanhpc1 <thanhpc1@newwave.vn>
2024-09-30 22:14:27 +0700

    Finish create SDG entity and relationship to Users

---
+++ FALOFinancialProofing/Models/SDG.cs
@@ -0,0 +1,13 @@
+﻿using FALOFinancialProofing.Core;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FALOFinancialProofing.Models
+{
+    public class SDG : Entity<int>
+    {
+        [ForeignKey("User")]
+        public string SDGName { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
--- FALOFinancialProofing/Models/User.cs
+++ FALOFinancialProofing/Models/User.cs
@@ -7,15 +7,16 @@ namespace FALOFinancialProofing.Models
     {
         public string FirstName { get; set; } = null!;
         public string LastName { get; set; } = null!;
-        //public DateOnly? BirthDate { get; set; }
-        //public string? Gender { get; set; }
-        //public string? Address { get; set; }
-        //public string? PhoneNumber { get; set; }
-        //public string Email { get; set; }
-        //public string SocialAddress { get; set; }
-        //public string? Avatar { get; set; }
-        //public string Password { get; set; }
-        //public int Status { get; set; }
-        //public virtual List<Role> Roles { get; set; }
-    }
+        public virtual SDG SDG { get; set; } 
+    //public DateOnly? BirthDate { get; set; }
+    //public string? Gender { get; set; }
+    //public string? Address { get; set; }
+    //public string? PhoneNumber { get; set; }
+    //public string Email { get; set; }
+    //public string SocialAddress { get; set; }
+    //public string? Avatar { get; set; }
+    //public string Password { get; set; }
+    //public int Status { get; set; }
+    //public virtual List<Role> Roles { get; set; }
+}
 }
