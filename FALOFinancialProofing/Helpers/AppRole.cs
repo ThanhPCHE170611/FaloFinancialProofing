caf2bdfcf967dc2dfff8ccf351648f836aa16f06
NGUYEN VAN DUC <ducnvhe160331@fpt.edu.vn>
2024-10-22 23:19:07 +0700

    AddSeed Role

--- FALOFinancialProofing/Helpers/AppRole.cs
+++ FALOFinancialProofing/Helpers/AppRole.cs
@@ -2,8 +2,10 @@
 {
     public class AppRole
     {
-        public const string Admin = "Admin";
-        public const string User = "User";
-        public const string HR = "Human Resources";
+        public const string ProjectManager = "Project Manager";
+        public const string VolunteerLeader = "Volunteer Leader";
+        public const string Accounting = "Accounting";
+        public const string Volunteer = "Volunteer";
+        //public const string HR = "Human Resources";
     }
 }
