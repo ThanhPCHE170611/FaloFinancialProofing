78235c78eb539dee1a59f433f6e0d77938e6eaeb
NguyenMinh <minhngquangg@gmail.com>
2024-10-30 17:46:47 +0700

    Convert Clientsite to Razor: Update ui in homepage, campaigndetails, projectdetails

---
+++ FALOFinancialProofing.FALOHomePage/Controllers/ProjectpageController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FALOFinancialProofing.FALOHomePage.Controllers
+{
+    public class ProjectpageController : Controller
+    {
+        // GET: ProjectpageController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ProjectpageController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProjectpageController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProjectpageController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProjectpageController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProjectpageController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProjectpageController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProjectpageController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
