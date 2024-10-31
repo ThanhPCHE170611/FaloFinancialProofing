d9d249cd068cebbdc9f53229876ba794e3e4406d
NGUYEN DAC HOANG DAT <datndhhe170720@fpt.edu.vn>
2024-10-31 15:57:37 +0700

    New_FE_Management

---
+++ FALOFinanacialProofing.Management/Controllers/PrepayController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FALOFinanacialProofing.Management.Controllers
+{
+    public class PrepayController : Controller
+    {
+        public ActionResult PrepayManagement()
+        {
+            return View();
+        }
+        public ActionResult PrepayManagement_PM()
+        {
+            return View();
+        }
+        public ActionResult PrepayCreate()
+        {
+            return View();
+        }
+        public ActionResult PrepayDetail()
+        {
+            return View();
+        }
+        public ActionResult PrepayVoucher()
+        {
+            return View();
+        }
+        public ActionResult PrepayVoucherCreate()
+        {
+            return View();
+        }
+
+
+
+        // GET: PrepayController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PrepayController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PrepayController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PrepayController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PrepayController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PrepayController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PrepayController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ FALOFinanacialProofing.Management/Controllers/UserProfileController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FALOFinanacialProofing.Management.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        public ActionResult UserProfileIndex()
+        {
+            return View();
+        }
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+
+
+        // GET: UserProfileController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserProfileController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserProfileController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UserProfileController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserProfileController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UserProfileController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
