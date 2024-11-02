b0405cf0fae55ebb757bee2ae583ac53aeecd1a5
NGUYEN DAC HOANG DAT <datndhhe170720@fpt.edu.vn>
2024-11-02 15:28:24 +0700

    ChooseRole_Add_API

--- FALOFinancialProofing/Resource.Designer.cs
+++ FALOFinancialProofing/Resource.Designer.cs
@@ -88,7 +88,7 @@ namespace FALOFinancialProofing {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to ProjectManager.
+        ///   Looks up a localized string similar to Project Manager.
         /// </summary>
         public static string ProjectManagerRoleName {
             get {
