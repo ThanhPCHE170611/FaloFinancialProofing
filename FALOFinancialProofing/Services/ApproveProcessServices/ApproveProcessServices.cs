285ba4c657f4c0c634cfbdb39bedaa7e2c53af28
NGUYEN DAC HOANG DAT <datndhhe170720@fpt.edu.vn>
2024-11-05 00:12:57 +0700

    Update ApproveProcessServices.cs

--- FALOFinancialProofing/Services/ApproveProcessServices/ApproveProcessServices.cs
+++ FALOFinancialProofing/Services/ApproveProcessServices/ApproveProcessServices.cs
@@ -382,7 +382,7 @@ namespace FALOFinancialProofing.Services.ApproveProcessServices
                 {
                     var requestForms = await requestFormRepository.GetAll(x => x.CampaignId == campaign.Id 
                         && x.TypeId == IntConstant.PaymentRequestType
-                        && !x.ApproveProcesses.Any(x => x.ApproverId.Equals(userid)))
+                        && x.ApproveProcesses.Any(x => x.ApproverId.Equals(userid)))
                         .Include(x => x.AttachmentFiles)
                         .Include(x => x.ApproveProcesses)
                         .Select(rf => new PrePayRequestFormViewRequest
