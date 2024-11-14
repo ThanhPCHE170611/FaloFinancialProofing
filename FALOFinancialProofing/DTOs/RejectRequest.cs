85117240f462e667eb62132b20bf53d77f139527
thanhpc1 <thanhpc1@newwave.vn>
2024-11-14 11:23:41 +0700

    update validate attribute for fetch api reject cho cả 3 role, api đăng ký, api tạo request payment và prepay

---
+++ FALOFinancialProofing/DTOs/RejectRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FALOFinancialProofing.DTOs
+{
+    public class RejectRequest
+    {
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        public string CurrentLoggingRole { get; set; }
+        [Required]
+        public int RequestId { get; set; }
+        [Required, MinLength(10)]
+        public string Feedback { get; set; }
+    }
+}
