47565cc614a54cc2c1285600b666e599a5c465a7
ThanhPCHE170611 <tpc23072003@gmail.com>
2024-11-26 23:55:28 +0700

    fix prepay payment

--- FALOFinancialProofing/Constant/IntConstant.cs
+++ FALOFinancialProofing/Constant/IntConstant.cs
@@ -3,9 +3,9 @@
     public static class IntConstant
     {
         // Page size for pagination default
-        public static readonly int PageSize = 5;
-        public static readonly int PageSizeCustom = 6;
-        public static readonly int PageSizeForDeb = 20;
+        public static readonly int PageSize = 30;
+        public static readonly int PageSizeCustom = 30;
+        public static readonly int PageSizeForDeb = 50;
 
         public const int PageNumberDefault = 1;
 
