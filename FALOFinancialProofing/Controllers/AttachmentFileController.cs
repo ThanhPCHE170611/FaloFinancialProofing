9ae75d28ba27bf9983de8af17bc0f3b9dda1ebc7
ThanhPCHE170611 <tpc23072003@gmail.com>
2024-11-28 21:23:57 +0700

    update search for view current prepay and payment request, update pagination default page size

--- FALOFinancialProofing/Controllers/AttachmentFileController.cs
+++ FALOFinancialProofing/Controllers/AttachmentFileController.cs
@@ -1,5 +1,6 @@
 ﻿using FALOFinancialProofing.Constant;
 using FALOFinancialProofing.Services.AttachmentFIleServices;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FALOFinancialProofing.Controllers
