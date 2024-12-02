8a6adbbb145d5df16821da986641e2780c789a7f
NGUYEN VAN DUC <ducnvhe160331@fpt.edu.vn>
2024-12-02 14:47:05 +0700

    Campaignmember

--- FALOFinancialProofing/Controllers/CampaignMemberController.cs
+++ FALOFinancialProofing/Controllers/CampaignMemberController.cs
@@ -224,11 +224,11 @@ namespace FALOFinancialProofing.Controllers
         }
         [RoleAttribute(AppRole.ProjectManager, AppRole.ProjectManagementBoard, AppRole.Admin)]
         [HttpPost("CreateManyCampaignMembers/{CampaignId}/{pmUserId}")]
-        public async Task<IActionResult> CreateManyCampaignMembers(int CampaignId, string pmUserId, [FromBody] List<CreateManyCampaignMemberDTO> createManyCampaignMemberDTOs)
+        public async Task<IActionResult> CreateManyCampaignMembers(int CampaignId, string pmUserId, string pmRoleId, [FromBody] List<CreateManyCampaignMemberDTO> createManyCampaignMemberDTOs)
         {
             var message = new StringBuilder();
 
-            var ValidCreateManyCampaignMemberDTOs = await _campaignMemberService.ValidateCampaignMembersCreateAsync(createManyCampaignMemberDTOs, CampaignId, pmUserId, message);
+            var ValidCreateManyCampaignMemberDTOs = await _campaignMemberService.ValidateCampaignMembersCreateAsync(createManyCampaignMemberDTOs, CampaignId, pmUserId, pmRoleId, message);
             if (ValidCreateManyCampaignMemberDTOs == null || ValidCreateManyCampaignMemberDTOs.Count == 0)
             {
                 return Ok(new ApiResponse()
