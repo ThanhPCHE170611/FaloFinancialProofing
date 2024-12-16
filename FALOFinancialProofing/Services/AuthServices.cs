47bb4d3f5a3769f0e0e7fa5e96f83e9fdd598179
ThanhPCHE170611 <tpc23072003@gmail.com>
2024-12-16 15:34:18 +0700

    remove link in forgot password password features

--- FALOFinancialProofing/Services/AuthServices.cs
+++ FALOFinancialProofing/Services/AuthServices.cs
@@ -760,12 +760,9 @@ namespace FALOFinancialProofing.Services
                 var token = await userManager.GeneratePasswordResetTokenAsync(user);
 
                 // Tạo nội dung email
-                string resetPasswordLink = "https://localhost:7109"; // Đường dẫn cố định
                 string emailContent = $@"
 Mã token của bạn: {token}
-Mã token này sẽ bị vô hiệu hóa sau 10 phút.
-
-Click vào link này để đặt lại mật khẩu: {resetPasswordLink}";
+Mã token này sẽ bị vô hiệu hóa sau 10 phút.";
 
                 // Create the email message
                 var message = new Message(
